7defe4fafe63e0ef526f91b3919d96d20f11bae9
Marianna Zurek <zurekmarianna@gmail.com>
2023-01-15 19:14:22 +0100

    uploaded cloudinary

--- server.js
+++ server.js
@@ -2,6 +2,28 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cloudinaryFramework from 'cloudinary';
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
+const cloudinary = cloudinaryFramework.v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'pets',
+    allowedFormats: ['jpg', 'png'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
+  },
+});
+
+const parser = multer({ storage });
 
 dotenv.config();
 
